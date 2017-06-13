d48893315c445032994c7bfc1bb7889d7a80a4bb
katamoto_kosuke <katamoto_kosuke@uniquevision.co.jp>
2017-06-13 18:57:34 +0900

    delete mail address

--- support.rb
+++ support.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require "selenium-webdriver"
 
 # Chrome用のドライバを指定
@@ -5,30 +6,17 @@ driver = Selenium::WebDriver.for :chrome
 
 #wantedlyにアクセス
 driver.navigate.to "https://www.wantedly.com"
-
 #ログインボタンを探す
 login_button = driver.find_element(:id, "root-login")
-
-
+#ログインボタンを押す->ポップアップみたいなのが出現
 login_button.click
-
-#wantedly
 login_pop = driver.find_element(:id, "signin-window")
-
-
+#ログイン実行
 login_pop.find_element(:class, "wt-ui-button").click
-
-
-#facebook 
+#facebookの画面と思われる
 login_form = driver.find_element(:id, "login_form")
-
-
-login_form.find_element(:name, "email").send_keys("09056931729")
-
-
-login_form.find_element(:name, "pass").send_keys("kosuke0605")
-
-
+login_form.find_element(:name, "email").send_keys("")
+login_form.find_element(:name, "pass").send_keys("")
 login_form.find_element(:id, "loginbutton").click
 p "----------1"
 driver.get "https://www.wantedly.com/projects/35855" #スーパー遅い
