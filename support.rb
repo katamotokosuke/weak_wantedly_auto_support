a6dab59f1c09ea504d8bccd71e3d8a8af1b1bf50
katamoto_kosuke <katamoto_kosuke@uniquevision.co.jp>
2017-06-13 20:24:19 +0900

    modify support rb

--- support.rb
+++ support.rb
@@ -2,10 +2,16 @@
 require "selenium-webdriver"
 
 # Chrome用のドライバを指定
-driver = Selenium::WebDriver.for :chrome
+# p "Facebookログインするぜ"
+# p "電話番号orメールアドレス"
+# fb_key = gets
+# p "パスワード"
+# fb_pass = gets
 
+driver = Selenium::WebDriver.for :chrome
+driver.manage.timeouts.implicit_wait = 60
 #wantedlyにアクセス
-driver.navigate.to "https://www.wantedly.com"
+driver.get "https://www.wantedly.com"
 #ログインボタンを探す
 login_button = driver.find_element(:id, "root-login")
 #ログインボタンを押す->ポップアップみたいなのが出現
@@ -15,27 +21,21 @@ login_pop = driver.find_element(:id, "signin-window")
 login_pop.find_element(:class, "wt-ui-button").click
 #facebookの画面と思われる
 login_form = driver.find_element(:id, "login_form")
-login_form.find_element(:name, "email").send_keys("")
-login_form.find_element(:name, "pass").send_keys("")
+login_form.find_element(:name, "email").send_keys("09056931729")
+login_form.find_element(:name, "pass").send_keys("kosuke0605")
 login_form.find_element(:id, "loginbutton").click
-p "----------1"
-driver.get "https://www.wantedly.com/projects/35855" #スーパー遅い
-p "-----------2"
+
+url_ary = ["97746", "3093"] #82977 105867 105873 4579 82984 82976
+url_ary.each do |url|
+	driver.get "https://www.wantedly.com/projects/#{url}"
+	p "1"
+	support_form = driver.find_element(:class, "project-supporters-list-wrapper")
+	p "2"
+	support_form.find_element(:class, "project-support-link").click
+	p "3"
+	support_pop = driver.find_element(:class, "dialog-actions-wrapper")
+	p "4"
+	support_pop.find_element(:class, "right").find_element(:class, "wt-ui-button").click
+	p "5"
+end
 driver.quit
-
-# support = driver.find_element(:class, "project-supporters-list-wrapper")
-
-
-
-# name属性にqが設定されている要素を取得
-#inputElement = driver.find_element(:name, 'q')
-
-# 取得した要素にパンケーキを設定
-#inputElement.send_keys "パンケーキ"
-
-# name属性にbtnGが設定されている要素を取得
-#submitElement = driver.find_element(:name, 'btnG')
-
-
-# テストを終了
-#driver.quit
